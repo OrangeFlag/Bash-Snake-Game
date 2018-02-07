36dc9a6656e089741cbe726dbda8583569a1c978
Mr.Orange <account_ilya@mail.ru>
2018-02-07 19:03:27 +0300

    Refactoring and add help

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#TODO refactoring, highscore table, readme, help, set level
+#TODO refactoring, highscore table, set level
 
 #– | \ / 
 
@@ -41,39 +41,43 @@ PushCommandLineParameters()
 
 Help()
 {
-	#echo help
+	echo    "Help:"
+	echo    "-l n  -  set level number"
+	echo    "-c n  -  count of food on map"  
+	echo	"-p n  -  waiting time for direction selection (example: -p 0.4)"
+	echo	"-n n  -  width and height of the field"
+	echo	"-h    -  print this help"
+	echo    "Example: ./start -c 5 -p 0.3 -n 20"
 	exit
 }
+
 SetLevel()
 {
 	#change settings
 	sleep 0
 }
+
 NewGameInit()
 {
-newGame="false"
-lastDirection=">"
-direction=">"
-turn="–"
-score=0
-countFood=0
-nextStep=0
-zmei=( $( [ $(($count/2)) -gt 0 ] && echo $(($count/2)) ) $( [ $(($count/2-1)) -gt 0 ] && echo $(($count/2-1)) ) $( [ $(($count/2-2)) -gt 0 ] && echo $(($count/2-2)) ) $( [ $(($count/2-3)) -gt 0 ] && echo $(($count/2-3)) ) $( [ $(($count/2-4)) -gt 0 ] && echo $(($count/2-4)) ) )
+	newGame="false"
+	lastDirection=">"
+	direction=">"
+	turn="–"
+	score=0
+	countFood=0
+	nextStep=0
+	zmei=( $( [ $(($count/2)) -gt 0 ] && echo $(($count/2)) ) $( [ $(($count/2-1)) -gt 0 ] && echo $(($count/2-1)) ) $( [ $(($count/2-2)) -gt 0 ] && echo $(($count/2-2)) ) $( [ $(($count/2-3)) -gt 0 ] && echo $(($count/2-3)) ) $( [ $(($count/2-4)) -gt 0 ] && echo $(($count/2-4)) ) )
 }
 
 Init()
 {
-#customizable
-maxCountFood=3
-n=8
-pause=0.25
-PushCommandLineParameters $1 $2 $3 $4 $5 $6 $7 $8 $9
-count=$(($n*$n))
-NewGameInit
-
+	maxCountFood=3
+	n=8
+	pause=0.25
+	PushCommandLineParameters $1 $2 $3 $4 $5 $6 $7 $8 $9
+	count=$(($n*$n))
+	NewGameInit
 }
-
-Init $1 $2 $3 $4 $5 $6 $7 $8 $9 
 
 ClearTable()
 {
@@ -100,22 +104,21 @@ ApplySnake()
 
 ReadKeyboard()
 {
-read -s -n 3 -t $pause key
-case $(echo $key | cat -v) in
-        '^[[A')
-                direction='^'
-        ;;
-        '^[[B')
-                direction='⌄'
-        ;;
-        '^[[C')
-                direction='>'
-        ;;
-        '^[[D')
-                direction='<'
-        ;;
-esac
-
+	read -s -n 3 -t $pause key
+	case $(echo $key | cat -v) in
+        	'^[[A')
+                	direction='^'
+	        ;;
+        	'^[[B')
+                	direction='⌄'
+	        ;;
+        	'^[[C')
+                	direction='>'
+	        ;;
+        	'^[[D')
+	                direction='<'
+        	;;
+	esac
 }
 
 NewFood()
@@ -132,6 +135,7 @@ NewFood()
 }
 
 
+Init $1 $2 $3 $4 $5 $6 $7 $8 $9 
 ClearTable
 ApplySnake
 
@@ -152,29 +156,30 @@ NewGame()
 
 GameOver()
 {
-echo -ne "\e[1;41m"
-echo  """   ____                       ___                 
+	echo -ne "\e[1;41m"
+	echo  """   ____                       ___                 
   / ___| __ _ _ __ ___   ___ / _ \\__   _____ _ __ 
  | |  _ / _\` | '_ \` _ \\ / _ | | | \\ \\ / / _ | '__|
  | |_| | (_| | | | | | |  __| |_| |\\ V |  __| |   
   \\____|\\__,_|_| |_| |_|\\___|\\___/  \\_/ \\___|_|   
                                                   """
-echo -ne "\e[0m"
-NewGame
+	echo -ne "\e[0m"
+	NewGame
 }
+
 Victory()
 {
-clear
-echo -en "\e[1;42m"
-echo """__     ___      _                   
+	clear
+	echo -en "\e[1;42m"
+	echo """__     ___      _                   
 \ \\   / (_) ___| |_ ___  _ __ _   _ 
  \\ \\ / /| |/ __| __/ _ \\| '__| | | |
   \\ V / | | (__| || (_) | |  | |_| |
    \\_/  |_|\\___|\\__\\___/|_|   \\__, |
                               |___/ 
 				    """
-echo -ne "\e[0m"
-NewGame
+	echo -ne "\e[0m"
+	NewGame
 }
 
 TestStep()
@@ -202,7 +207,6 @@ TestGameOver()
 	fi
 }
 
-
 SnakeTurn()
 {
 	if [[ $direction = '>' && $lastDirection = '⌄' ||  $direction = '⌄' && $lastDirection = '>' ||  $direction = '<' && $lastDirection = '^' ||  $direction = '^' && $lastDirection = '<' ]]
@@ -228,89 +232,88 @@ SnakeTurn()
 
 while [ true ]
 do
-clear
+	clear
 
-if [ $countFood -lt $maxCountFood ]
-then
-	NewFood
-fi
-
-
-#--------------------------------------------------print table and score
-last_j=0
-for (( c=0;c<$count;c++ ))
-do
-	echo -ne "${color[$c]}"
-	echo -ne "${pole[$c]}"
-	echo -ne "\e[0m"
-	let "j=($c+1)/$n"
-	
-	if [ $last_j -ne $j ]
+	if [ $countFood -lt $maxCountFood ]
 	then
-		echo
-		last_j=$j
+		NewFood
 	fi
-done
-echo -e "Score: \e[1m$score\e[0m"
-#--------------------------------------------------print table and score ~
 
 
-lastDirection=$direction
-ReadKeyboard
-
-#--------------------------------------------------calculate zmeika
-if [[ $direction = '>' && $lastDirection = '<' || $direction = '<' && $lastDirection = '>' || $direction = '^' && $lastDirection = '⌄' || $direction = '⌄' && $lastDirection = '^' ]]
-then
-	direction=$lastDirection
-fi
-
-
-case $direction in 
-	">")
-		nextStep=$(( (count + zmei[0] + 1 - ( (zmei[0]+1)/$n - zmei[0]/$n)*$n ) %count ))
-	;;
-	"<")
-		nextStep=$(( (count + zmei[0] - 1 + (zmei[0]/$n - (zmei[0]-1)/$n)*$n) % count ))
-	;;
-	"^")
-		nextStep=$(( (count + zmei[0] - $n) % count))
-	;;
-	"⌄")
-		nextStep=$(( (zmei[0] + $n) % count))
-	;;
-esac
-
-TestStep
-if [[ "$newGame" = "true" ]]
-then
-	newGame="false"
-	continue
-fi
-SnakeTurn
-
-pole[${zmei[0]}]="$turn" #change old head
-
-
-if [[ "${zmei[$((${#zmei[@]}-1))]}" != 'forDelete' ]]
-then
-	pole[${zmei[$((${#zmei[@]}-1))]}]="*" #delete old tail
-	color[${zmei[$((${#zmei[@]}-1))]}]="\e[0m"
+	#--------------------------------------------------print table and score
+	last_j=0
+	for (( c=0;c<$count;c++ ))
+	do	
+		echo -ne "${color[$c]}"
+		echo -ne "${pole[$c]}"
+		echo -ne "\e[0m"
+		let "j=($c+1)/$n"
 	
-fi
+		if [ $last_j -ne $j ]
+		then
+			echo
+			last_j=$j
+		fi
+	done
+	echo -e "Score: \e[1m$score\e[0m"
+	#--------------------------------------------------print table and score ~
 
-TestGameOver
 
-if [[ "$newGame" = "true" ]]
-then
-	newGame="false"
-	continue
-fi
+	lastDirection=$direction
+	ReadKeyboard
 
-zmei[$((${#zmei[@]}-1))]="" #delete old tail
+	#--------------------------------------------------calculation snake
+	if [[ $direction = '>' && $lastDirection = '<' || $direction = '<' && $lastDirection = '>' || $direction = '^' && $lastDirection = '⌄' || $direction = '⌄' && $lastDirection = '^' ]]
+	then
+		direction=$lastDirection
+	fi
 
-zmei=( "$nextStep"  ${zmei[@]} )
-pole[${zmei[0]}]="$direction" #new head
-color[${zmei[0]}]="\e[1;32m"
-#--------------------------------------------------calculate zmeika ~
 
+	case $direction in 
+		">")
+			nextStep=$(( (count + zmei[0] + 1 - ( (zmei[0]+1)/$n - zmei[0]/$n)*$n ) %count ))
+		;;
+		"<")
+			nextStep=$(( (count + zmei[0] - 1 + (zmei[0]/$n - (zmei[0]-1)/$n)*$n) % count ))
+		;;
+		"^")
+			nextStep=$(( (count + zmei[0] - $n) % count))
+		;;
+		"⌄")
+			nextStep=$(( (zmei[0] + $n) % count))
+		;;
+	esac
+
+	TestStep
+	if [[ "$newGame" = "true" ]]
+	then
+		newGame="false"
+		continue
+	fi
+	SnakeTurn
+
+	pole[${zmei[0]}]="$turn" #change old head
+
+
+	if [[ "${zmei[$((${#zmei[@]}-1))]}" != 'forDelete' ]]
+	then
+		pole[${zmei[$((${#zmei[@]}-1))]}]="*" #delete old tail
+		color[${zmei[$((${#zmei[@]}-1))]}]="\e[0m"
+	
+	fi
+
+	TestGameOver
+
+	if [[ "$newGame" = "true" ]]
+	then
+		newGame="false"
+		continue
+	fi
+
+	zmei[$((${#zmei[@]}-1))]="" #delete old tail
+
+	zmei=( "$nextStep"  ${zmei[@]} )
+	pole[${zmei[0]}]="$direction" #new head
+	color[${zmei[0]}]="\e[1;32m"
+	#--------------------------------------------------calculation snake~
 done
